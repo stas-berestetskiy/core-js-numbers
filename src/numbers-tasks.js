f581efa4c75251828c5d990c5810d69ec275a8d0
Stanislav Berestetskiy <stasberestetskiy@gmail.com>
2023-11-16 00:11:14 +0100

    3 tasks solved

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -51,8 +51,9 @@ function getCircleCircumference(radius) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+  const average = value1 / 2 + value2 / 2;
+  return average;
 }
 
 /**
